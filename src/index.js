6f082e4528fb38de72e9e98033b6dfe141085f2a
성진옥 <wlsdhr0831@naver.com>
2021-10-07 13:19:40 +0900

    test router

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename="/">
     <App/>
   </BrowserRouter>,
   document.getElementById('app')
