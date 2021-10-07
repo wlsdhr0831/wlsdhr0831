af0f15534dbd47b9ca06e7270ab590975c548b33
성진옥 <wlsdhr0831@naver.com>
2021-10-07 13:23:09 +0900

    test route

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter basename="/">
+  <BrowserRouter basename="/wlsdhr0831">
     <App/>
   </BrowserRouter>,
   document.getElementById('app')
