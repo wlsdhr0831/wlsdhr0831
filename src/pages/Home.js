dd687bf2f4cfb4138e6d36f6287d8f122cb27c1e
성진옥 <wlsdhr0831@naver.com>
2021-10-17 00:58:30 +0900

    change profile img

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,14 @@ import Search from "../components/home/Search";
 import List from "../components/home/List";
 import SearchInput from "../components/home/SearchInput";
 import Button from "../components/common/Button";
+import Image1 from "../img/1.jpg";
+import Image2 from "../img/2.jpg";
+import Image3 from "../img/3.jpg";
 
 const Home = () => {
   return (<>
     <ProfileImage 
-      src="https://cdn.mkhealth.co.kr/news/photo/202004/img_MKH200424005_0.jpg"/>
+      src={Image1} alt="Profile Image"/>
     <Search>
       <SearchInput/>
       <Button>검색</Button>
