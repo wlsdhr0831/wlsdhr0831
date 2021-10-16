9cca92d2c374e00e230ab4274999d6ed2ced0e9b
성진옥 <wlsdhr0831@naver.com>
2021-10-16 12:10:08 +0900

    modify devServer proxy

--- webpack.config.js
+++ webpack.config.js
@@ -38,11 +38,5 @@ module.exports = {
   ],
   devServer: {
     historyApiFallback: true, 
-    proxy: {
-      "/" : {
-          target: "http:170.0.0.1:8000/",
-          changeOrigin: true
-      }
-    }
   }
 };
